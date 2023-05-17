a1d36b4e2edefe6663ac7f80b5b21c22f0d1a689
EhsanUddoula <121718110+EhsanUddoula@users.noreply.github.com>
2023-05-17 23:46:07 +0600

    Delete java directory

--- java/com/example/Controller/aboutController.java
+++
@@ -1,9 +0,0 @@
-package com.example.Controller;
-
-import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
-
-public class aboutController {
-    @FXML
-    private AnchorPane about_pane;
-}
